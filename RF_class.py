f3473949ffed8fa6fde62ac58c88d91fe8438e00
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2025-11-11 14:43:59 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -2094,7 +2094,7 @@ class RF:
                 elif "_restart" in message_text:
                     print("Получена команда перезапуска")
                     await event.message.delete()  # Удаляем сообщение
-                    msg = await self.client.send_message(event.chat_id, "Ver.5.11.11")
+                    msg = await self.client.send_message(event.chat_id, "Ver.6.11.11")
                     await asyncio.sleep(5)
                     await msg.delete()  # Удаляем сообщение о версии
                     await asyncio.sleep(1)
@@ -2604,7 +2604,7 @@ class RF:
                     if member_id != self.cave_leader_id:
                         await self.client.send_message(member_id, "Выходим из пещеры _фольт")
                         print(f"Отправлено сообщение участнику {member_id}: Выходим из пещеры _фольт")
-                await asyncio.sleep(20) 
+                await asyncio.sleep(25) 
                 await self.rf_message.click(3)
         else:
             print(f"Ещё рано на выход. Общее здоровье: {total_health}, Живых: {alive_count}")
