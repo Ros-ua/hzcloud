ee46cf3588e420a3ed5a8f98602e4e88bba8cb17
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2025-12-26 16:17:12 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -50,7 +50,7 @@ class RF:
         self.bezvgroup = -1002220238697
         self.group59 = -1001323974021
         self.location = "🔥 61-65 Лес пламени"  # Локация по умолчанию
-        self.version = "Ver.peremennaya.26.12"
+        self.version = "Elka.26.12"
         # === КОНФИГ И ВЫЧИСЛЕНИЯ ===
         self.pvp_binds = RF_config.pvp_binds
         self.hp_binds = RF_config.hp_binds
@@ -1850,6 +1850,7 @@ class RF:
                     await self.send_command( "⛏ Заснеженный Рудник")
                 await asyncio.sleep(900)  # 15 минут = 900 секунд
                 self.def_rudnik = False
+                self.after_caves = False
                 if not self.is_in_caves and not self.waiting_for_captcha:
                     await self.send_command(RF.hp)  # Переодеться для мобов
                 # # await self.wait_for_set_change() #работает
