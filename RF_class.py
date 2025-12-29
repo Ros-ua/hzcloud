b747b2561eb588d29776b0050bbde6c342b9d4d3
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2025-12-29 20:51:06 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -50,7 +50,7 @@ class RF:
         self.bezvgroup = -1002220238697
         self.group59 = -1001323974021
         self.location = "🔥 61-65 Лес пламени"  # Локация по умолчанию
-        self.version = "3.29.12"
+        self.version = "4.29.12"
         # === КОНФИГ И ВЫЧИСЛЕНИЯ ===
         self.pvp_binds = RF_config.pvp_binds
         self.hp_binds = RF_config.hp_binds
@@ -1167,6 +1167,7 @@ class RF:
                         await self.send_command("/drink_102")
                         self.mob_drink_counter -= 1
                         print(f"Выпито /drink_102, осталось: {self.mob_drink_counter}")
+                        await self.client.send_message(self.cave_leader_id, f"осталось выпить {self.mob_drink_counter} банок")
                         await asyncio.sleep(1)
                         await self.send_command(self.location)
                     else:
