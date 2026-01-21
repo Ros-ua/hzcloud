a545a8ec518e8d7d45989ad80f629f979523af7e
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-01-21 12:15:47 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -50,7 +50,7 @@ class RF:
         self.bezvgroup = -1002220238697
         self.group59 = -1001323974021
         self.location = "🔥 61-65 Лес пламени"  # Локация по умолчанию
-        self.version = "K.17.01"
+        self.version = "F.21.01"
         # === КОНФИГ И ВЫЧИСЛЕНИЯ ===
         self.pvp_binds = RF_config.pvp_binds
         self.hp_binds = RF_config.hp_binds
@@ -2879,16 +2879,16 @@ class RF:
             if self.is_cave_leader:
                 for member_id in group_members:
                     if member_id != self.cave_leader_id:
-                        await asyncio.sleep(15)
+                        await asyncio.sleep(5)
                         await self.client.send_message(member_id, "Выходим из пещеры _фольт")
                         print(f"Отправлено сообщение участнику {member_id}: Выходим из пещеры _фольт")
-                await asyncio.sleep(10)
+                await asyncio.sleep(5)
                 # Надеваем фольт на себя после ожидания перед кликом выхода
                 if hasattr(self, "folt_binds") and self.folt_binds:
                     print("Отправляем команду folt_binds для себя")
                     await self.send_command(self.folt_binds[0][1])
                     await self.wait_for_set_change()
-                    await asyncio.sleep(1)
+                    await asyncio.sleep(2)
                 await self.rf_message.click(3)
         else:
             print(f"Ещё рано на выход. Общее здоровье: {total_health}, Живых: {alive_count}")
