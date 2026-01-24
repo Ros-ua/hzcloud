8806f41a95155b8f0c36633fc582b7f560613e42
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-01-24 22:49:43 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -51,9 +51,9 @@ class RF:
         self.bezvgroup = -1002220238697
         self.group59 = -1001323974021
         self.location = "🔥 61-65 Лес пламени"  # Локация по умолчанию
-        if your_name == "Лучшее_что_было_в_моей_жизни-RF":
+        if self.your_name == "Лучшее_что_было_в_моей_жизни-RF":
             self.location = "🦇 51-60 Земли Изгнанников"
-        self.version = "LOC.23.01"
+        self.version = "LOC.24.01"
         # === КОНФИГ И ВЫЧИСЛЕНИЯ ===
         self.pvp_binds = RF_config.pvp_binds
         self.hp_binds = RF_config.hp_binds
