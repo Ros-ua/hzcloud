fd44edbe999dd61c2891c0d38fccf13b67fb3177
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-02-14 20:17:06 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -35,7 +35,7 @@ class RF:
         # === ВСЕ ЧТО РАВНО NONE ===
         self.cave_buttons_message = self.elka_active = self.last_command = self.killed_on_chv = self.rf_message = self.last_talisman_info = self.cmd_altar = self.last_bind = self.after_bind = self.last_set_kingRagnar = self.move_timer = self.last_energy_message = self.got_reward = self.terminal_type = self.steps = self.cave_message_id = self.last_step = self.current_location = self.drink_status_message_id = self.group_members = None
         # === ЧИСЛА ===
-        self.version = "wbt.11.01"
+        self.version = "kop.14.02"
         self.vex_bot_id = 1033007754
         self.bot_id = 577009581
         self.tomat_id = 278339710
@@ -2005,7 +2005,7 @@ class RF:
             if any("Война окончена!" in ln for ln in lines):
                 await asyncio.sleep(70)
                 self.def_rudnik = True
-                if not self.is_moving and not self.killed_on_chv and not self.is_in_caves:
+                if not self.is_moving and not self.killed_on_chv and not self.is_in_caves and not self.kopka:
                     await self.send_command( "⛏Рудник")
                 await asyncio.sleep(900)  # 15 минут = 900 секунд
                 self.def_rudnik = False
