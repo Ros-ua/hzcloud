4ba3e06074a60ab27061a8ce6bba071ef636fc02
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-02-28 16:14:46 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -35,7 +35,7 @@ class RF:
         # === ВСЕ ЧТО РАВНО NONE ===
         self.cave_buttons_message = self.elka_active = self.last_command = self.killed_on_chv = self.rf_message = self.last_talisman_info = self.cmd_altar = self.last_bind = self.after_bind = self.last_set_kingRagnar = self.move_timer = self.last_energy_message = self.got_reward = self.terminal_type = self.steps = self.cave_message_id = self.last_step = self.current_location = self.drink_status_message_id = self.group_members = None
         # === ЧИСЛА ===
-        self.version = "5hpba.27.02"
+        self.version = "900.28.02"
         self.vex_bot_id = 1033007754
         self.bot_id = 577009581
         self.tomat_id = 278339710
@@ -1994,7 +1994,7 @@ class RF:
                     # "Лучшее_что_было_в_моей_жизни-RF",
                     # "ИмяТретье",
                 ):
-                    await asyncio.sleep(15)
+                    await asyncio.sleep(900)
                     await self.send_command("/vote_register")
             elif any("Война окончена!" in ln for ln in lines):
                 await asyncio.sleep(70)
