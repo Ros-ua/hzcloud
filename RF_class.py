1e1ed982e59111be9cc19960966711fa1911343d
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-03-01 22:18:46 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -35,7 +35,7 @@ class RF:
         # === ВСЕ ЧТО РАВНО NONE ===
         self.cave_buttons_message = self.elka_active = self.last_command = self.killed_on_chv = self.rf_message = self.last_talisman_info = self.cmd_altar = self.last_bind = self.after_bind = self.last_set_kingRagnar = self.move_timer = self.last_energy_message = self.got_reward = self.terminal_type = self.steps = self.cave_message_id = self.last_step = self.current_location = self.drink_status_message_id = self.group_members = None
         # === ЧИСЛА ===
-        self.version = "900.28.02"
+        self.version = "lines.1.3"
         self.vex_bot_id = 1033007754
         self.bot_id = 577009581
         self.tomat_id = 278339710
@@ -1916,7 +1916,17 @@ class RF:
         async def on_war_start(event):
             if self.waiting_for_captcha:
                 return  # Выходим из функции, ничего не обрабатываем
-            lines = event.message.text.splitlines()
+            # lines = event.message.text.splitlines()
+
+            # Берём последние 2 сообщения из канала
+            messages = await self.client.get_messages(-1001284047611, limit=2)
+            now = datetime.datetime.now(datetime.timezone.utc)
+            lines = []
+            for msg in messages:
+                age = (now - msg.date).total_seconds()
+                if age <= 5:
+                    lines.extend((msg.text or "").splitlines())
+
             # Проверка на ядерный удар по локации
             if any("Ядерный удар по локации" in ln for ln in lines):
                 line = next(ln for ln in lines if "Ядерный удар по локации" in ln)
