66cb050fbefb904895b8f7bdaa9fbe0b14250d07
Ros-ua <51153745+Ros-ua@users.noreply.github.com>
2026-03-30 16:24:38 +0800

    Add files via upload

--- RF_class.py
+++ RF_class.py
@@ -35,7 +35,7 @@ class RF:
         # === ВСЕ ЧТО РАВНО NONE ===
         self.cave_buttons_message = self.elka_active = self.last_command = self.killed_on_chv = self.rf_message = self.last_talisman_info = self.cmd_altar = self.last_bind = self.after_bind = self.last_set_kingRagnar = self.move_timer = self.last_energy_message = self.got_reward = self.terminal_type = self.steps = self.cave_message_id = self.last_step = self.current_location = self.drink_status_message_id = self.group_members = None
         # === ЧИСЛА ===
-        self.version = "regist.25.3"
+        self.version = "sec.30.3"
         self.vex_bot_id = 1033007754
         self.bot_id = 577009581
         self.tomat_id = 278339710
@@ -1650,7 +1650,7 @@ class RF:
                         if any("Здоровье пополнено" in line for line in lstr):
                             await asyncio.sleep(0.1)
                             return
-            await asyncio.sleep(1)
+            await asyncio.sleep(2)
     async def wait_for_set_change(self):
         # Ожидание смены сета
         while True:
